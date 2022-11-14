c02d74c819acfdc747ac958a54fabcc686736ff6
Kateryna Tsygankova <kateryna.tsygankova.se@gmail.com>
2022-11-14 19:14:09 +0200

    js added

--- project.js
+++ project.js
@@ -1,3 +1,13 @@
-function onSubmit(event){
-alert
+
+function handleSubmit(event){
+event.preventDefault();
+    if(clientName.value.length > 0 && email.value.length > 2){
+            alert("Welcome! We'll contact you within 24 hours")
+        } else {alert ("Something is not right. Please check the form")}
 }
+
+let clientName = document.querySelector("#name");
+let email = document.querySelector("#email");
+
+let subscribeButton = document.querySelector("form");
+subscribeButton.addEventListener("submit", handleSubmit);
